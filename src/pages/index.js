f430906b6a3235ba64e8dd8c841410fac59ed132
Romain PAILLER <romain.pailler@equasens.com>
2025-08-20 13:46:27 +0200

    modif

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={`${siteConfig.title}`} description="Rapport d'alternance PAILLER Romain 2024-2025">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
